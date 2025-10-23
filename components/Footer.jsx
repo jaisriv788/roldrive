d660a24b4e7d2e0a15d609d1ab2347b9289f6f0c
jaisriv788 <srivastavajai845@gmail.com>
2025-10-24 01:34:39 +0530

    feat: added navbar and footer design in the page along with responsive design

---
+++ components/Footer.jsx
@@ -0,0 +1,215 @@
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <div className="bg-[#081017] py-12 px-5 sm:px-14 text-white">
+      <div className="flex justify-between lg:flex-row flex-col">
+        <div className="lg:max-w-1/5 flex flex-col gap-3">
+          <Image
+            src="/navbar/Frame.svg"
+            alt="Navbar frame"
+            width={130}
+            height={45}
+            unoptimized
+          />
+          <div className="text-gray-400 text-[14px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore.
+          </div>
+          <div className="flex gap-3 lg:flex-col">
+            <div className="text-gray-200">+44 (0) 207 112 8101</div>
+            <div className="text-gray-200">booking@roldrive.com</div>
+          </div>
+          <div className="flex gap-3">
+            <Image
+              className="cursor-pointer hover:scale-110 transition ease-in-out duration-300"
+              src="/footer/facebook.svg"
+              alt="Icon"
+              width={24}
+              height={45}
+              unoptimized
+            />
+            <Image
+              className="cursor-pointer hover:scale-110 transition ease-in-out duration-300"
+              src="/footer/x.svg"
+              alt="Icon"
+              width={24}
+              height={45}
+              unoptimized
+            />
+            <Image
+              className="cursor-pointer hover:scale-110 transition ease-in-out duration-300"
+              src="/footer/instagram.svg"
+              alt="Icon"
+              width={24}
+              height={45}
+              unoptimized
+            />
+            <Image
+              className="cursor-pointer hover:scale-110 transition ease-in-out duration-300"
+              src="/footer/pinterest.svg"
+              alt="Icon"
+              width={24}
+              height={45}
+              unoptimized
+            />
+            <Image
+              className="cursor-pointer hover:scale-110 transition ease-in-out duration-300"
+              src="/footer/linkedin.svg"
+              alt="Icon"
+              width={24}
+              height={45}
+              unoptimized
+            />
+          </div>
+        </div>
+
+        <div className="lg:hidden h-px bg-gray-500 my-5"></div>
+
+        <div className="flex-1 flex">
+          <div className="flex w-full justify-around">
+            <div className="flex flex-col gap-1.5">
+              <div className="font-bold">Fleet</div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Business
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                First
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Luxury
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Electric
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                SUV
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                MVP
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Sprinter
+              </div>
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <div className="font-bold">Services</div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Airport Transfers
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Road Shows
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Intercity Transfers
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Event Transfers
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                All Services
+              </div>
+            </div>
+            <div className="lg:hidden hidden sm:flex flex-col gap-1.5">
+              <div className="font-bold">Top Cities</div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                London
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Paris
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                New York
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Dubai
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Tokyo
+              </div>
+            </div>
+            <div className="lg:hidden hidden sm:flex flex-col gap-1.5">
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                FAQ
+              </div>
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Blogs
+              </div>
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Career
+              </div>
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                About Us
+              </div>
+              <div className="font-bold text-[#FDC65C] cursor-pointer hover:text-[#8e630d]">
+                Become A Supplier
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="lg:hidden h-px bg-gray-500 my-5"></div>
+
+        <div className="flex-1 lg:flex sm:hidden">
+          <div className="flex w-full justify-around">
+            <div className="flex flex-col gap-1.5">
+              <div className="font-bold">Top Cities</div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                London
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Paris
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                New York
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Dubai
+              </div>
+              <div className=" hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Tokyo
+              </div>
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                FAQ
+              </div>
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Blogs
+              </div>
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                Career
+              </div>
+              <div className="font-bold hover:text-gray-400 cursor-pointer transition ease-in-out duration-300">
+                About Us
+              </div>
+              <div className="font-bold text-[#FDC65C] cursor-pointer hover:text-[#8e630d]">
+                Become A Supplier
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-px bg-gray-500 my-5 sm:hidden"></div>
+      </div>
+
+      <div className="mt-8 mb-3 flex gap-2 text-sm sm:text-base sm:gap-3 text-gray-400">
+        <span className="hover:text-gray-500 cursor-pointer transition ease-in-out duration-300">
+          Privacy Policy
+        </span>
+        <span className="hover:text-gray-500 cursor-pointer transition ease-in-out duration-300">
+          GDPR Policy
+        </span>
+        <span className="hover:text-gray-500 cursor-pointer transition ease-in-out duration-300">
+          Terms & Condition
+        </span>
+      </div>
+      <div className="h-px bg-gray-500"></div>
+      <div className="mt-3 mb-8 flex gap-3 text-gray-400 text-sm sm:text-base">
+        Copyright ©2024 - RolDrive. All Rights Reserved 
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,137 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuShortcut,
+} from "@/components/ui/dropdown-menu";
+import { RiMenuFill } from "react-icons/ri";
+
+function Navbar() {
+  return (
+    <div className="bg-[#223544] fixed w-screen text-white py-5 px-5 sm:px-14 flex justify-between items-center">
+      <div className="flex items-center gap-5">
+        <Link href="/">
+          <Image
+            src="/navbar/Frame.svg"
+            alt="Navbar frame"
+            width={155}
+            height={50}
+            className="w-[120px] sm:w-[155px] h-auto"
+            unoptimized
+          />
+        </Link>
+
+        <div className="hidden lg:flex gap-3">
+          <Link className="hover:text-gray-300 transition ease-in-out duration-300" href="/services">
+            Services
+          </Link>
+          <Link className="hover:text-gray-300 transition ease-in-out duration-300" href="/fleet">
+            Fleet
+          </Link>
+          <Link className="hover:text-gray-300 transition ease-in-out duration-300" href="/businesssolutions">
+            Business Solutions
+          </Link>
+          <Link className="hover:text-gray-300 transition ease-in-out duration-300" href="/cities">
+            Cities
+          </Link>
+          <Link className="hover:text-gray-300 transition ease-in-out duration-300" href="/airporttransfers">
+            Airport Transfers
+          </Link>
+        </div>
+      </div>
+      <div className="hidden lg:flex items-center gap-3">
+        <div className="flex gap-2 cursor-pointer hover:text-gray-300 transition ease-in-out duration-300">
+          <Image
+            src="/navbar/whatsapp.svg"
+            alt="Navbar frame"
+            width={20}
+            height={20}
+          />
+          Contact Us
+        </div>
+        <Select defaultValue="english" disableScrollLock>
+          <SelectTrigger className="w-[70px] border border-gray-500">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="english">En</SelectItem>
+            <SelectItem value="hindi">Hn</SelectItem>
+          </SelectContent>
+        </Select>
+        <button className="bg-white text-black px-5 py-1.5 rounded-lg hover:bg-gray-300 cursor-pointer font-semibold transition ease-in-out duration-300">
+          Sign In
+        </button>
+      </div>
+      <div className="flex lg:hidden gap-5 items-center">
+        <Select defaultValue="english">
+          <SelectTrigger className="w-[65px] border border-gray-500">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="english">En</SelectItem>
+            <SelectItem value="hindi">Hn</SelectItem>
+          </SelectContent>
+        </Select>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <RiMenuFill size={20} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-60" align="start">
+            <DropdownMenuLabel>Navigation</DropdownMenuLabel>
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Link href="/services">Services</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/fleet">Fleet</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/businesssolutions">Business Solutions</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/cities">Cities</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/airporttransfers">Airport Transfers</Link>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuLabel>Others</DropdownMenuLabel>
+
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="cursor-pointer">
+                Contact Us
+                <DropdownMenuShortcut>
+                  <Image
+                    src="/navbar/whatsapp.svg"
+                    alt="Navbar frame"
+                    width={18}
+                    height={18}
+                  />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                Sign In
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
